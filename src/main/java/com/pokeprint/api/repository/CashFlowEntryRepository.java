cb773e4165e1ae014c8cfd1cfc2a49bee4556705
Enzo <enzo@example.com>
2026-09-23 01:29:55 -0300

    Remove orphan CashFlowEntry entity

--- src/main/java/com/pokeprint/api/repository/CashFlowEntryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pokeprint.api.repository;
-
-import com.pokeprint.api.domain.entity.CashFlowEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CashFlowEntryRepository extends JpaRepository<CashFlowEntry, Long> {
-}
